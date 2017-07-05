0fc0e83c0f976c2d71b86bf671ab863a091f4a4a
negifunyai <negifunyai@gmail.com>
2017-07-05 23:29:09 +0900

    first oniisama

---
+++ README.md
@@ -0,0 +1,10 @@
+これなに？
+------
+
+さすがはお兄様です
+
+
+インストール
+------
+
+``git clone git://github.com/negifunyai/sasuoni.git ~/.mikutter/plugin/``
---
+++ sasuoni.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+Plugin.create(:sasuoni) do
+  command(:sasuoni,
+          name: 'さすおに',
+          condition: Plugin::Command[:CanReaplyAll],
+          visible: true,
+          role: :timeline
+         ) do |opt|
+      
+    message = opt.messages.first
+    screen_name = message.user[:idname]
+    message.post(:message => "さすがは" + "@#{screen_name}様です")
+  end 
+end
